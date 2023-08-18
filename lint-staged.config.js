b2472e6f2067d8f4409955c73ec60258c271b2f0
Peng <94596408+peng-few@users.noreply.github.com>
2023-08-18 19:40:19 +0800

    feat: test jest file before commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -6,4 +6,5 @@ module.exports = {
         files => `nx affected:lint --files=${files.join(',')}`,
         files => `nx format:write --files=${files.join(',')}`,
     ],
+    'packages/**/*.spec.{ts,tsx,js}': 'test:staged'
 };
