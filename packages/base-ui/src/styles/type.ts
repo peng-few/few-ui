4e1bc047141e9c26dc4d838fb5cd13f6c93941ab
Peng <94596408+peng-few@users.noreply.github.com>
2023-09-02 00:29:06 +0800

    feat: add currentcolor to Color type

--- packages/base-ui/src/styles/type.ts
+++ packages/base-ui/src/styles/type.ts
@@ -2,4 +2,11 @@ export type Rgb = `rgb(${number}, ${number}, ${number})`;
 export type Rgba = `rgba(${number}, ${number}, ${number}, ${number})`;
 export type Hex = `#${string}`;
 
-export type Color = Rgb | Rgba | Hex | 'transparent' | 'white' | 'black';
+export type Color =
+  | Rgb
+  | Rgba
+  | Hex
+  | 'transparent'
+  | 'white'
+  | 'black'
+  | 'currentcolor';
