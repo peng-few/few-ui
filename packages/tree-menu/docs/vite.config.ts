0eb7a30ee724c763b766eaab0d17d83ca08a8132
Peng <94596408+peng-few@users.noreply.github.com>
2023-09-07 15:50:34 +0800

    fix: emotion vite config

--- packages/tree-menu/docs/vite.config.ts
+++ packages/tree-menu/docs/vite.config.ts
@@ -20,7 +20,9 @@ export default defineConfig({
   },
 
   plugins: [
-    react(),
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
     viteTsConfigPaths({
       root: '../../../',
     }),
